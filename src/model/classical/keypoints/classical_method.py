2ad88c27f9a726085274fc87b2a02b027343f96e
Alexandru Meterez <ameterez@student.ethz.ch>
2022-04-11 16:16:01 +0200

    rewrote the extractor

--- src/model/classical/keypoints/classical_method.py
+++ src/model/classical/keypoints/classical_method.py
@@ -4,9 +4,18 @@ import os
 import argparse
 import time
 import scipy
+import plotly
+import plotly.graph_objects as go
+from tqdm import tqdm
+from plotly.subplots import make_subplots 
 from scipy.optimize import curve_fit
 np.set_printoptions(suppress=True)
 np.set_printoptions(precision=3)
+
+def objective(X, a0, a1, a2, a3, a4, a5):
+	x = X[:, 0]
+	y = X[:, 1]
+	return a0 * (x ** 2) + a1 * (y ** 2) + a2 * (x * y) + a3 * x + a4 * y + a5
 
 class Extractor(object):
 	def __init__(self, fragment_path, output_path, keypoint_radius, r_values, n_keypoints):
@@ -37,122 +46,117 @@ class Extractor(object):
 		fragment = np.load(self.fragment_path)
 		point_cloud = fragment[:, :3]
 		normals = fragment[:, 3:]
+		point_cloud = np.asarray(point_cloud, order='F')
+		normals = np.asarray(normals, order='F')
 
 		# Get all radius r neighbourhoods for each r
 		keypoint_radius = self.keypoint_radius
 		tree = spatial.KDTree(point_cloud)
 
 		# Extract keypoints
-		neighbourhood = tree.query_ball_point(point_cloud, keypoint_radius, workers=-1)
-		SD = self.get_SD_for_point_cloud(point_cloud, normals, neighbourhood)
+		nbhd = tree.query_ball_point(point_cloud, keypoint_radius, workers=-1)
+		SD = self.get_SD_for_point_cloud(point_cloud, normals, nbhd)
 
 		# Extract keypoint indices
 		keypoint_indices = np.argsort(np.abs(SD))[-self.n_keypoints:]
 
-		# Extract keypoints
-		keypoints = point_cloud[keypoint_indices]
-		keypoints_normals = normals[keypoint_indices]
-		# For each r value
-		s = len(self.r_vals)
-
-		# Output matrix
-		output_matrix = np.zeros((len(keypoints), 3 + 3 + s * 49))
-		output_matrix[:, :3] = keypoints
-		output_matrix[:, 3:6] = keypoints_normals
-
-		for r_idx, r in enumerate(self.r_vals):
-			print(f"Fragment {fragment_path} with radius {r}")
-			r_neighbourhood = tree.query_ball_point(point_cloud, r, workers=-1)
-
-			# For each point p_i in the nbhd of a keypoint p, compute the matrix and the features
-			for p_idx in range(len(keypoints)):
-				# The matrix with all the features, where each row is a phi_i (point in nbhd of p)
-				Phi = np.zeros((len(r_neighbourhood[keypoint_indices[p_idx]]), 7))
-				
-				p = keypoints[p_idx]
-				p_neighbourhood = np.asarray(r_neighbourhood[keypoint_indices[p_idx]])
-				# Compute C_p
-				p_bar = np.mean(point_cloud[p_neighbourhood], axis=1)
-				P = point_cloud[p_neighbourhood].T
-				C_p = (P - p_bar) @ (P - p_bar).T
-
-				# Do SVD on C_p
-				U, S, Vt = np.linalg.svd(C_p)
-				lambda1, lambda2, lambda3 = S
-
-				# Compute deltas and save them in matrix
-				delta1 = (lambda1 - lambda2) / lambda1
-				delta2 = (lambda2 - lambda3) / lambda1
-				delta3 = lambda3 / lambda1
-				Phi[:, 3] = delta1
-				Phi[:, 4] = delta2
-				Phi[:, 5] = delta3
-				# Compute cosine for each keypoint (vectorized, p_i are the points in the nbhd of p)
-				# print(point_cloud[p_neighbourhood].shape, p.shape)
-				p_pi = point_cloud[p_neighbourhood] - p
-				n_pi = normals[p_neighbourhood]
-				n_p = normals[keypoint_indices[p_idx]]
-				for i in range(len(p_neighbourhood)):					
-					cos_alpha_i = np.dot(p_pi[i], n_pi[i]) / np.linalg.norm(p_pi)
-					cos_beta_i = np.dot(p_pi[i], n_p) / np.linalg.norm(p_pi)
-					cos_gamma_i = np.dot(n_pi[i], n_p)
-
-					Phi[i, 0] = cos_alpha_i
-					Phi[i, 1] = cos_beta_i
-					Phi[i, 2] = cos_gamma_i
-				# Compute H
-				xdata = point_cloud[p_neighbourhood][:, :2]
-				ydata = point_cloud[p_neighbourhood][:, 2]
-
-
-				# define the true objective function
-				def objective(X, a0, a1, a2, a3, a4, a5):
-					x = X[:, 0]
-					y = X[:, 1]
-					return a0 * (x ** 2) + a1 * (y ** 2) + a2 * (x * y) + a3 * x + a4 * y + a5
-
-				popt, _ = curve_fit(objective, xdata, ydata, p0=[0,0,0,0,0,0])
-				a0, a1, a2, a3, a4, a5 = popt
-
-				# (x, y, f(x, y))
-				# r_x = (1, 0, d/dx f)
-				# r_xy = (0, 0, d/dxdy f)
-				# r_y = (0, 1, d/dy f)
-				# r_xx = (0, 0, d/dxdx f)
-				# r_yy = (0, 0, d/dydy f)
-				
-				r_x = np.zeros((3))
-				r_xy = np.zeros((3))
-				r_xx = np.zeros((3))
-				r_y = np.zeros((3))
-				r_yy = np.zeros((3))
-				
-				r_x[2] = 2 * a0 * p[0] + a2 * p[1] + a3
-				r_xx[2] = 2 * a0
-				r_xy[2] = a2
-				r_y[2] = 2 * a1 * p[1] + a2 * p[0] + a4
-				r_yy[2] = 2 * a1
-				r_x[0] = 1
-				r_y[1] = 1
-				E = np.dot(r_x, r_x)
-				F = np.dot(r_x, r_y)
-				G = np.dot(r_y, r_y)
-				L = np.dot(r_xx, n_p)
-				M = np.dot(r_xy, n_p)
-				N = np.dot(r_yy, n_p)
-				H = (E * N - 2 * F * M + G * L) / (2 * (E * G - F ** 2))
-
-				# Set the value
-				Phi[:, 6] = H
-
-				# Now we have Phi, gotta compute the last part (cov mat)
-				Phi_bar = np.mean(Phi, axis=0)
-				centered_Phi = Phi - Phi_bar
-				C_r = centered_Phi.T @ centered_Phi / (len(p_neighbourhood))
-				output_matrix[p_idx, 6 + r_idx * 49: 6 + (r_idx + 1) * 49] = C_r.ravel()
-		# print(output_matrix)
-		output_matrix[:, 6:] = np.log(output_matrix[:, 6:])
+		# Compute the neighbourhoods in all r vals 
+		neighbourhoods = {}
+		for r in self.r_vals:
+			neighbourhoods[r] = tree.query_ball_point(point_cloud, r, workers=-1)
 		
+		# Output
+		output_matrix = np.zeros((len(keypoint_indices), 3 + 3 + 49 * len(self.r_vals)))
+
+		# For each keypoint
+		for n_keypoint, keypoint_index in enumerate(tqdm(keypoint_indices)):
+			# Get keypoint and normal of the keypoint
+			keypoint = point_cloud[keypoint_index]
+			keypoint_normal = normals[keypoint_index]
+
+			# Set output matrix
+			output_matrix[n_keypoint, :3] = keypoint
+			output_matrix[n_keypoint, 3:6] = keypoint_normal
+
+			# For each radius r, compute the matrix with the features
+			for r_idx, r in enumerate(self.r_vals):
+				# Get neighbourhood of keypoint
+				keypoint_neighbourhood = neighbourhoods[r][keypoint_index]
+
+				# Initialize Phi
+				Phi = []
+
+				# For each point in the keypoint neighbourhood
+				for idx, p_i in enumerate(keypoint_neighbourhood):
+					if p_i == keypoint_index:
+						continue
+					phi_i = []
+
+					# Get neighbourhood of p_i
+					p_i_neighbourhood = neighbourhoods[r][p_i]
+
+					# Compute cosines
+					p_i_point = point_cloud[p_i]
+					p_i_normal = normals[p_i]
+					p_p_i_vector = p_i_point - keypoint
+					cos_alpha = np.dot(p_p_i_vector, p_i_normal) / np.linalg.norm(p_p_i_vector)
+					cos_beta = np.dot(p_p_i_vector, keypoint_normal) / np.linalg.norm(p_p_i_vector)
+					cos_gamma = np.dot(p_i_normal, keypoint_normal)
+					phi_i.append(cos_alpha)
+					phi_i.append(cos_beta)
+					phi_i.append(cos_gamma)
+
+					# Compute C_p_i and delta1, delta2, delta3
+					C_p_i = np.cov(point_cloud[p_i_neighbourhood].T) * len(p_i_neighbourhood)
+					U, S, Vt = np.linalg.svd(C_p_i)
+					lambda1, lambda2, lambda3 = S
+					delta1 = (lambda1 - lambda2) / lambda1
+					delta2 = (lambda2 - lambda3) / lambda1
+					delta3 = lambda3 / lambda1
+					phi_i.append(delta1)
+					phi_i.append(delta2)
+					phi_i.append(delta3)
+
+					# Compute H
+					p_i_neighbourhood_points = point_cloud[p_i_neighbourhood]
+					xdata = p_i_neighbourhood_points[:, :2]
+					ydata = p_i_neighbourhood_points[:, 2]
+					popt, _ = curve_fit(objective, xdata, ydata, p0=[0,0,0,0,0,0])
+					a0, a1, a2, a3, a4, a5 = popt
+					r_x = np.zeros((3))
+					r_xy = np.zeros((3))
+					r_xx = np.zeros((3))
+					r_y = np.zeros((3))
+					r_yy = np.zeros((3))
+					r_x[2] = 2 * a0 * p_i_point[0] + a2 * p_i_point[1] + a3
+					r_xx[2] = 2 * a0
+					r_xy[2] = a2
+					r_y[2] = 2 * a1 * p_i_point[1] + a2 * p_i_point[0] + a4
+					r_yy[2] = 2 * a1
+					r_x[0] = 1
+					r_y[1] = 1
+					E = np.dot(r_x, r_x)
+					F = np.dot(r_x, r_y)
+					G = np.dot(r_y, r_y)
+					L = np.dot(r_xx, p_i_normal)
+					M = np.dot(r_xy, p_i_normal)
+					N = np.dot(r_yy, p_i_normal)
+					H = (E * N - 2 * F * M + G * L) / (2 * (E * G - F ** 2))
+
+					# Set the value
+					phi_i.append(H)
+
+					Phi.append(phi_i)
+				Phi = np.array(Phi)
+				C_r = np.cov(Phi.T)
+
+				# Compute log and save
+				S, R = np.linalg.eig(C_r)
+				S += 1e-3
+				S = np.log(S)
+				log_C_r = R @ np.diag(S) @ R.T
+				output_matrix[n_keypoint, 6 + r_idx * 49: 6 + (r_idx + 1) * 49] = log_C_r.ravel()
+
 		np.save(self.output_path, output_matrix)
 
 if __name__ == '__main__':
