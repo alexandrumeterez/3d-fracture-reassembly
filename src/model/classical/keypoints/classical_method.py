9d84cad5dc1ad932bb0e972660355f458e80bc7e
Alexandru Meterez <ameterez@student.ethz.ch>
2022-04-18 17:29:33 +0200

    dumb testing

--- src/model/classical/keypoints/classical_method.py
+++ src/model/classical/keypoints/classical_method.py
@@ -26,6 +26,7 @@ class Extractor(object):
 		self.r_vals = r_values
 		self.keypoint_radius = keypoint_radius
 		self.n_keypoints = n_keypoints
+		self.cov_mats = []
 
 	def compute_SD_point(self, neighbourhood, points, normals, p_idx):
 		p_i = points[p_idx]
@@ -48,6 +49,7 @@ class Extractor(object):
 		fragment = np.load(self.fragment_path)
 		point_cloud = fragment[:, :3]
 		normals = fragment[:, 3:]
+		self.point_cloud = point_cloud
 		point_cloud = np.asarray(point_cloud, order='F')
 		normals = np.asarray(normals, order='F')
 
@@ -61,12 +63,12 @@ class Extractor(object):
 
 		# Extract keypoint indices
 		keypoint_indices = np.argsort(np.abs(SD))[-self.n_keypoints:]
-
+		self.keypoints = point_cloud[keypoint_indices]
 		# Compute the neighbourhoods in all r vals 
 		neighbourhoods = {}
 		for r in self.r_vals:
 			neighbourhoods[r] = tree.query_ball_point(point_cloud, r, workers=-1)
-		
+
 		# Output
 		output_matrix = np.zeros((len(keypoint_indices), 3 + 3 + 49 * len(self.r_vals)))
 
@@ -88,76 +90,33 @@ class Extractor(object):
 				# Initialize Phi
 				Phi = []
 
-				# For each point in the keypoint neighbourhood
-				for idx, p_i in enumerate(keypoint_neighbourhood):
-					if p_i == keypoint_index:
+				for p_i_idx in keypoint_neighbourhood:
+					p_i = point_cloud[p_i_idx]
+					v = p_i - keypoint
+
+					if np.linalg.norm(v) < 1e-5:
 						continue
-					phi_i = []
+					
+					cos_alpha = np.dot(v, normals[p_i_idx])
+					cos_alpha /= np.linalg.norm(v)
 
-					# Get neighbourhood of p_i
-					p_i_neighbourhood = neighbourhoods[r][p_i]
+					cos_beta = np.dot(v, keypoint_normal)
+					cos_beta /= np.linalg.norm(v)
 
-					# Compute cosines
-					p_i_point = point_cloud[p_i]
-					p_i_normal = normals[p_i]
-					p_p_i_vector = p_i_point - keypoint
-					cos_alpha = np.dot(p_p_i_vector, p_i_normal) / np.linalg.norm(p_p_i_vector)
-					cos_beta = np.dot(p_p_i_vector, keypoint_normal) / np.linalg.norm(p_p_i_vector)
-					cos_gamma = np.dot(p_i_normal, keypoint_normal)
-					phi_i.append(cos_alpha)
-					phi_i.append(cos_beta)
-					phi_i.append(cos_gamma)
+					cos_gamma = np.dot(normals[p_i_idx], keypoint_normal)
 
-					# Compute C_p_i and delta1, delta2, delta3
-					C_p_i = np.cov(point_cloud[p_i_neighbourhood].T) * len(p_i_neighbourhood)
-					U, S, Vt = np.linalg.svd(C_p_i)
-					lambda1, lambda2, lambda3 = S
-					delta1 = (lambda1 - lambda2) / lambda1
-					delta2 = (lambda2 - lambda3) / lambda1
-					delta3 = lambda3 / lambda1
-					phi_i.append(delta1)
-					phi_i.append(delta2)
-					phi_i.append(delta3)
-
-					# Compute H
-					p_i_neighbourhood_points = point_cloud[p_i_neighbourhood]
-					xdata = p_i_neighbourhood_points[:, :2]
-					ydata = p_i_neighbourhood_points[:, 2]
-					popt, _ = curve_fit(objective, xdata, ydata, p0=[0,0,0,0,0,0])
-					a0, a1, a2, a3, a4, a5 = popt
-					r_x = np.zeros((3))
-					r_xy = np.zeros((3))
-					r_xx = np.zeros((3))
-					r_y = np.zeros((3))
-					r_yy = np.zeros((3))
-					r_x[2] = 2 * a0 * p_i_point[0] + a2 * p_i_point[1] + a3
-					r_xx[2] = 2 * a0
-					r_xy[2] = a2
-					r_y[2] = 2 * a1 * p_i_point[1] + a2 * p_i_point[0] + a4
-					r_yy[2] = 2 * a1
-					r_x[0] = 1
-					r_y[1] = 1
-					E = np.dot(r_x, r_x)
-					F = np.dot(r_x, r_y)
-					G = np.dot(r_y, r_y)
-					L = np.dot(r_xx, p_i_normal)
-					M = np.dot(r_xy, p_i_normal)
-					N = np.dot(r_yy, p_i_normal)
-					H = (E * N - 2 * F * M + G * L) / (2 * (E * G - F ** 2))
-
-					# Set the value
-					phi_i.append(H)
-
+					phi_i = [cos_alpha, cos_beta, cos_gamma]
 					Phi.append(phi_i)
-				Phi = np.array(Phi)
-				C_r = np.cov(Phi.T)
 
-				# Compute log and save
-				S, R = np.linalg.eig(C_r)
-				S += 1e-3
+				Phi = np.asarray(Phi)
+				Phi = Phi.T
+				cov_mat = np.cov(Phi)
+
+				S, R = np.linalg.eig(cov_mat)
 				S = np.log(S)
-				log_C_r = R @ np.diag(S) @ R.T
-				output_matrix[n_keypoint, 6 + r_idx * 49: 6 + (r_idx + 1) * 49] = log_C_r.ravel()
+				cov_mat = R @ np.diag(S) @ R.T
+
+				self.cov_mats.append(cov_mat)
 
 		np.save(self.output_path, output_matrix)
 def f(fragment_path, output_path, keypoint_radius, r_vals, n_keypoints):
@@ -165,26 +124,118 @@ def f(fragment_path, output_path, keypoint_radius, r_vals, n_keypoints):
 	extractor.extract()
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--dataset_dir", type=str)
-	parser.add_argument("--keypoint_radius", type=float, required=True)
-	parser.add_argument("--n_keypoints", type=int, required=True)
-	parser.add_argument("--r_vals", nargs='+', required=True, type=float)
+	# parser = argparse.ArgumentParser()
+	# parser.add_argument("--dataset_dir", type=str)
+	# parser.add_argument("--keypoint_radius", type=float, required=True)
+	# parser.add_argument("--n_keypoints", type=int, required=True)
+	# parser.add_argument("--r_vals", nargs='+', required=True, type=float)
 
-	args = parser.parse_args()
-	fragments = os.listdir(args.dataset_dir)
-	fragments = [x for x in fragments if x.endswith(".npy")]
+	# args = parser.parse_args()
+	n_points = 2000
+	extractor1 = Extractor("/Users/alex/3dv_mockup/Venus/venus_part01.npy", ".", 0.1, [0.2], n_points)
+	extractor1.extract()
+	extractor2 = Extractor("/Users/alex/3dv_mockup/Venus/venus_part03.npy", ".", 0.1, [0.2], n_points)
+	extractor2.extract()
+	colors1 = ['blue' for _ in range(n_points)]
+	colors2 = ['blue' for _ in range(n_points)]
+	x_lines = []
+	y_lines = []
+	z_lines = []
+	c = 1
 
-	f_args = []
+	for i in range(n_points):
+		for j in range(n_points):
+			d = np.linalg.norm(extractor1.cov_mats[i] - extractor2.cov_mats[j], ord='fro')
+			if d < 0.3:
+				colors1[i] = 'green'
+				colors2[j] = 'green'
 
-	for fragment in fragments:
-		print(f"Fragment: {fragment}")
-		fragment_path = os.path.join(args.dataset_dir, fragment)
-		keypoints_dir = os.path.join(args.dataset_dir, "keypoints")
-		if not os.path.exists(keypoints_dir):
-			os.mkdir(keypoints_dir)
+				x_lines.append(extractor1.keypoints[i][0])
+				x_lines.append(extractor2.keypoints[j][0]+c)
+				x_lines.append(None)
 
-		output_path = os.path.join(keypoints_dir, fragment)
-		f_args.append((fragment_path, output_path, args.keypoint_radius, args.r_vals, args.n_keypoints))
-	with Pool(processes=8) as pool:
-		pool.starmap(f, f_args)
+				y_lines.append(extractor1.keypoints[i][1])
+				y_lines.append(extractor2.keypoints[j][1])
+				y_lines.append(None)
+
+				z_lines.append(extractor1.keypoints[i][2])
+				z_lines.append(extractor2.keypoints[j][2])
+				z_lines.append(None)
+
+	fig = go.Figure()
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor1.point_cloud[:, 0],
+			y = extractor1.point_cloud[:, 1],
+			z = extractor1.point_cloud[:, 2],
+			mode='markers',
+			marker=dict(
+				size=1,
+			)
+		)
+	)
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor1.keypoints[:, 0],
+			y = extractor1.keypoints[:, 1],
+			z = extractor1.keypoints[:, 2],
+			mode='markers',
+			marker=dict(
+				size=3,
+				color=colors1
+			)
+		)
+	)
+	extractor2.keypoints[:, 0] += c
+	extractor2.point_cloud[:, 0] += c
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor2.point_cloud[:, 0],
+			y = extractor2.point_cloud[:, 1],
+			z = extractor2.point_cloud[:, 2],
+			mode='markers',
+			marker=dict(
+				size=1,
+			)
+		)
+	)
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor2.keypoints[:, 0],
+			y = extractor2.keypoints[:, 1],
+			z = extractor2.keypoints[:, 2],
+			mode='markers',
+			marker=dict(
+				size=3,
+				color=colors2
+			)
+		)
+	)
+
+	fig.add_trace(
+		go.Scatter3d(
+			x = x_lines,
+			y = y_lines,
+			z = z_lines,
+			mode='lines',
+		)
+	)
+	fig.show()
+	
+
+	# fragments = os.listdir(args.dataset_dir)
+	# fragments = [x for x in fragments if x.endswith(".npy")]
+
+	# f_args = []
+
+	# for fragment in fragments:
+	# 	print(f"Fragment: {fragment}")
+	# 	fragment_path = os.path.join(args.dataset_dir, fragment)
+	# 	keypoints_dir = os.path.join(args.dataset_dir, "keypoints")
+	# 	if not os.path.exists(keypoints_dir):
+	# 		os.mkdir(keypoints_dir)
+
+	# 	output_path = os.path.join(keypoints_dir, fragment)
+	# 	f_args.append((fragment_path, output_path, args.keypoint_radius, args.r_vals, args.n_keypoints))
+	# with Pool(processes=8) as pool:
+	# 	pool.starmap(f, f_args)
