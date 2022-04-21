f8e55c359ab95f8f257a7d778ac9d866c20f052f
Alexandru Meterez <ameterez@student.ethz.ch>
2022-04-21 22:20:58 +0200

    added visualization tool

--- src/model/classical/keypoints/classical_method.py
+++ src/model/classical/keypoints/classical_method.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from plotly.subplots import make_subplots 
 from scipy.optimize import curve_fit
 from multiprocessing import Pool
-
+import pandas as pd
 np.set_printoptions(suppress=True)
 np.set_printoptions(precision=3)
 
@@ -26,6 +26,9 @@ class Extractor(object):
 		self.r_vals = r_values
 		self.keypoint_radius = keypoint_radius
 		self.n_keypoints = n_keypoints
+		self.cov_mats = []
+		self.point_cloud = None
+		self.keypoints = None
 
 	def compute_SD_point(self, neighbourhood, points, normals, p_idx):
 		p_i = points[p_idx]
@@ -47,6 +50,7 @@ class Extractor(object):
 	def extract(self):
 		fragment = np.load(self.fragment_path)
 		point_cloud = fragment[:, :3]
+		self.point_cloud = point_cloud
 		normals = fragment[:, 3:]
 		point_cloud = np.asarray(point_cloud, order='F')
 		normals = np.asarray(normals, order='F')
@@ -59,9 +63,12 @@ class Extractor(object):
 		nbhd = tree.query_ball_point(point_cloud, keypoint_radius, workers=-1)
 		SD = self.get_SD_for_point_cloud(point_cloud, normals, nbhd)
 
+		# Fix normals
+		normals = normals * np.sign(SD[:, None])
+
 		# Extract keypoint indices
 		keypoint_indices = np.argsort(np.abs(SD))[-self.n_keypoints:]
-
+		self.keypoints = self.point_cloud[keypoint_indices]
 		# Compute the neighbourhoods in all r vals 
 		neighbourhoods = {}
 		for r in self.r_vals:
@@ -71,7 +78,7 @@ class Extractor(object):
 		output_matrix = np.zeros((len(keypoint_indices), 3 + 3 + 49 * len(self.r_vals)))
 
 		# For each keypoint
-		for n_keypoint, keypoint_index in enumerate(keypoint_indices):
+		for n_keypoint, keypoint_index in enumerate(tqdm(keypoint_indices)):
 			# Get keypoint and normal of the keypoint
 			keypoint = point_cloud[keypoint_index]
 			keypoint_normal = normals[keypoint_index]
@@ -79,6 +86,7 @@ class Extractor(object):
 			# Set output matrix
 			output_matrix[n_keypoint, :3] = keypoint
 			output_matrix[n_keypoint, 3:6] = keypoint_normal
+			keypoint_cov_mats = []
 
 			# For each radius r, compute the matrix with the features
 			for r_idx, r in enumerate(self.r_vals):
@@ -154,37 +162,150 @@ class Extractor(object):
 
 				# Compute log and save
 				S, R = np.linalg.eig(C_r)
-				S += 1e-3
 				S = np.log(S)
 				log_C_r = R @ np.diag(S) @ R.T
+				keypoint_cov_mats.append(log_C_r)
 				output_matrix[n_keypoint, 6 + r_idx * 49: 6 + (r_idx + 1) * 49] = log_C_r.ravel()
-
+			self.cov_mats.append(keypoint_cov_mats)
 		np.save(self.output_path, output_matrix)
 def f(fragment_path, output_path, keypoint_radius, r_vals, n_keypoints):
 	extractor = Extractor(fragment_path, output_path, keypoint_radius, r_vals, n_keypoints)
 	extractor.extract()
 
+def visualize_matches(extractor1, extractor2, n_points, n_scales, threshold):
+	x_lines = []
+	y_lines = []
+	z_lines = []
+	c = 1
+	colors1 = ['blue' for _ in range(n_points)]
+	colors2 = ['blue' for _ in range(n_points)]
+	min_d = 99
+	for i in range(n_points):
+		for j in range(n_points):
+			d = 0.0
+			for s in range(n_scales):
+				d += np.linalg.norm(extractor1.cov_mats[i][s] - extractor2.cov_mats[j][s], ord='fro')
+			d /= n_scales
+			if d < min_d:
+				min_d = d
+				print(f"Min: {min_d}")
+			if d < threshold:
+				colors1[i] = 'green'
+				colors2[j] = 'green'
+
+				x_lines.append(extractor1.keypoints[i][0])
+				x_lines.append(extractor2.keypoints[j][0]+c)
+				x_lines.append(None)
+
+				y_lines.append(extractor1.keypoints[i][1])
+				y_lines.append(extractor2.keypoints[j][1])
+				y_lines.append(None)
+
+				z_lines.append(extractor1.keypoints[i][2])
+				z_lines.append(extractor2.keypoints[j][2])
+				z_lines.append(None)
+
+	fig = go.Figure()
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor1.point_cloud[:, 0],
+			y = extractor1.point_cloud[:, 1],
+			z = extractor1.point_cloud[:, 2],
+			mode='markers',
+			marker=dict(
+				size=1,
+			)
+		)
+	)
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor1.keypoints[:, 0],
+			y = extractor1.keypoints[:, 1],
+			z = extractor1.keypoints[:, 2],
+			mode='markers',
+			marker=dict(
+				size=3,
+				color=colors1
+			)
+		)
+	)
+
+	extractor2.keypoints[:, 0] += c
+	extractor2.point_cloud[:, 0] += c
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor2.point_cloud[:, 0],
+			y = extractor2.point_cloud[:, 1],
+			z = extractor2.point_cloud[:, 2],
+			mode='markers',
+			marker=dict(
+				size=1,
+			)
+		)
+	)
+	fig.add_trace(
+		go.Scatter3d(
+			x = extractor2.keypoints[:, 0],
+			y = extractor2.keypoints[:, 1],
+			z = extractor2.keypoints[:, 2],
+			mode='markers',
+			marker=dict(
+				size=3,
+				color=colors2
+			)
+		)
+	)
+
+	fig.add_trace(
+		go.Scatter3d(
+			x = x_lines,
+			y = y_lines,
+			z = z_lines,
+			mode='lines',
+		)
+	)
+	fig.show()
+	
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
+	parser.add_argument("--mode", default=1, type=int)
 	parser.add_argument("--dataset_dir", type=str)
 	parser.add_argument("--keypoint_radius", type=float, required=True)
 	parser.add_argument("--n_keypoints", type=int, required=True)
 	parser.add_argument("--r_vals", nargs='+', required=True, type=float)
-
+	parser.add_argument("--threshold", type=float, default=1.0, required=True, type=float)
+	parser.add_argument("--fragment1", type=str)
+	parser.add_argument("--fragment2", type=str)
 	args = parser.parse_args()
-	fragments = os.listdir(args.dataset_dir)
-	fragments = [x for x in fragments if x.endswith(".npy")]
 
-	f_args = []
+	if args.mode == 1:
+		fragments = os.listdir(args.dataset_dir)
+		fragments = [x for x in fragments if x.endswith(".npy")]
 
-	for fragment in fragments:
-		print(f"Fragment: {fragment}")
-		fragment_path = os.path.join(args.dataset_dir, fragment)
-		keypoints_dir = os.path.join(args.dataset_dir, "keypoints")
-		if not os.path.exists(keypoints_dir):
-			os.mkdir(keypoints_dir)
+		f_args = []
 
-		output_path = os.path.join(keypoints_dir, fragment)
-		f_args.append((fragment_path, output_path, args.keypoint_radius, args.r_vals, args.n_keypoints))
-	with Pool(processes=8) as pool:
-		pool.starmap(f, f_args)
+		for fragment in fragments:
+			print(f"Fragment: {fragment}")
+			fragment_path = os.path.join(args.dataset_dir, fragment)
+			keypoints_dir = os.path.join(args.dataset_dir, "keypoints")
+			if not os.path.exists(keypoints_dir):
+				os.mkdir(keypoints_dir)
+
+			output_path = os.path.join(keypoints_dir, fragment)
+			f_args.append((fragment_path, output_path, args.keypoint_radius, args.r_vals, args.n_keypoints))
+		with Pool(processes=8) as pool:
+			pool.starmap(f, f_args)
+	elif args.mode == 2:
+		# Just for visualization purposes
+
+		r = args.keypoint_radius
+		scales = args.r_vals
+		n_points = args.n_keypoints
+		extractor1 = Extractor(args.fragment1, "temp", r, scales, n_points)
+		extractor1.extract()
+		extractor2 = Extractor(args.fragment2, "temp", r, scales, n_points)
+		extractor2.extract()
+
+		visualize_matches(extractor2, extractor1, n_points, len(scales), args.threshold)
+
